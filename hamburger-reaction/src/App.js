dfa39d42c5abc1ad1151e098de4c5780ce15bb73
Giwa Uthman <uthmangiwa@ymail.com>
2019-05-27 13:00:42 +0100

    add layout and burger ingredients component

--- hamburger-reaction/src/App.js
+++ hamburger-reaction/src/App.js
@@ -1,10 +1,14 @@
 import React,{Component} from 'react';
+import Layout from './components/Layout/layout';
+import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render(){
     return (
       <div>
-        <p>Welcome</p>
+      <Layout>
+      <BurgerBuilder />
+      </Layout>
       </div>
     )
   }
---
+++ hamburger-reaction/src/container/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Auxi from '../../hoc/auxi'
+
+class BurgerBuilder extends Component{
+
+    render(){
+        return (
+        <Auxi>
+            <div>Burger</div>
+            <div>Burger Controls</div>
+        </Auxi>
+        )
+    }
+}
+
+export default BurgerBuilder;
