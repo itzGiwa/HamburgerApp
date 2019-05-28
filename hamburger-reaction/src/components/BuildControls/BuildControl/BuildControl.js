d88ad2a4a00ca5022c949a0334b153dcd253632b
Giwa Uthman <uthmangiwa@ymail.com>
2019-05-28 14:49:07 +0100

    Fix css issues

--- hamburger-reaction/src/components/BuildControls/BuildControl/BuildControl.js
+++ hamburger-reaction/src/components/BuildControls/BuildControl/BuildControl.js
@@ -2,11 +2,12 @@ import React from 'react';
 import classes from './BuilControl.css';
 
 const buildControl = (props) => {
- return ( <div>
-     <div className={classes.BuildControl}>{props.label}</div>
-     <button className={classes.More} onClick={props.more}>More</button>
+ return (
+     <div className={classes.BuildControl}>{props.label}
      <button className={classes.Less} onClick={props.less}>Less</button>
- </div> )
+     <button className={classes.More} onClick={props.more}>More</button>
+     </div>
+ )
 }
 
 export default buildControl
